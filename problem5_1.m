36427eaf0957d8fcd2f684ea32991ef5e2875dfc
Andrew Chung <68363626+andrewchungg@users.noreply.github.com>
2020-11-04 13:46:32 -0800

    Create problem5_1.m

---
+++ problem5_1.m
@@ -0,0 +1,21 @@
+
+EA1 = 406;
+d1 = -17.8 * pi/180;
+EA1 = EA1 * (cos(d1) + j * sin(d1));
+Xs = 0.6;
+Vp = 480;
+EA2 = (0.9:0.01:1.7) * 406;
+
+d2 = asin(abs(EA1)./abs(EA2).*sin(d1));
+
+EA2 = EA2 .* (cos(d2) + j .* sin(d2));
+
+IA2 = (Vp-EA2) / (j*Xs);
+
+figure(1)
+    plot(abs(EA2),abs(IA2))
+    xlabel('E_{A} (V)')
+    ylabel('I_{A} (A)')
+    title('Synchronous Motor V-Curve')
+    grid on;
+    
